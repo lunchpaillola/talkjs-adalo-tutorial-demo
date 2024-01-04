8b82921ffef863aaf0225d6825776a5e80e316e0
Lola-Ojabowale <lola.ojabowale@gmail.com>
2024-01-04 14:53:47 -0500

    react native updates

--- src/components/TalkJs/index.js
+++ src/components/TalkJs/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
+import sha256 from "crypto-js/sha256";
 import * as TalkRn from '@talkjs/react-native';
 
 const TalkJs = (props) => {
-	const { talkJsApplicationID, userId, name, email, photo, participantList, role } =props;
+	const {
+		editor,
+		talkJsApplicationID,
+		userId,
+		name,
+		email,
+		photo,
+		participantList,
+		role,
+} = props;
+const ID = talkJsApplicationID;
 
  const me = {
 		id: userId,
@@ -12,25 +23,57 @@ const TalkJs = (props) => {
 		photoUrl: photo.uri,
 };
 
-const ID = talkJsApplicationID;
-const other = {
-	id: 987654321,
-	name: "Sarah",
-	email: "lola.ojabowale@gmail.com",
-	photoUrl: "https://talkjs.com/images/avatar-5.jpg",
+if (!talkJsApplicationID || !userId) {
+	return (
+			<View style={styles.centeredLoader}>
+					<ActivityIndicator size="large" color="#0000ff" />
+			</View>
+	);
+}
+
+const createUniqueConversationId = (participantList) => {
+	const userIds = Array.from(
+			new Set(
+					participantList.map(
+							(participant) => participant?.participantDetails?.pUserId
+					)
+			)
+	).sort((a, b) => a - b);
+
+	const uniqueSortedIds = Array.from(new Set(userIds)).sort();
+	const concatenatedIds = uniqueSortedIds.join("-");
+	const hash = sha256(concatenatedIds);
+
+	return hash.toString();
 };
 
-const conversationBuilder = TalkRn.getConversationBuilder(
-	TalkRn.oneOnOneId(me, other)
-);
+const addParticipantsToConversation = (conversation, participantList) => {
+	if (participantList && participantList.length > 0) {
+			participantList.forEach((participantDetails) => {
+					const participant = new TalkRn.User({
+							id: participantDetails?.participantDetails?.pUserId,
+							name: participantDetails?.participantDetails?.pName,
+							email: participantDetails?.participantDetails?.pEmail,
+							photoUrl: participantDetails?.participantDetails?.pPhoto?.uri,
+							role: participantDetails?.participantDetails?.pRole,
+					});
+					conversation.setParticipant(participant);
+			});
+	}
+};
 
-conversationBuilder.setParticipant(me);
-conversationBuilder.setParticipant(other);
+const conversationId = createUniqueConversationId(participantList);
+const conversation = TalkRn.getConversationBuilder(conversationId);
+
+conversation.setParticipant(me);
+addParticipantsToConversation(conversation, participantList);
+
+
 
 	return(
 		<View style={styles.wrapper}>
 			<TalkRn.Session appId={ID} me={me}>
-			<TalkRn.Chatbox conversationBuilder={conversationBuilder} />
+			<TalkRn.Chatbox conversationBuilder={conversation} />
 			</TalkRn.Session>
 		</View>
 	)
